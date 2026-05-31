0cebd9b4c3d168ebdcc8fabc8e0cdea8eed198a6
Tien <phamt33@wwu.edu>
2026-05-30 18:37:57 -0700

    Added email requirement/checking when submitting email

--- emailTest.js
+++ emailTest.js
@@ -8,22 +8,42 @@ window.onload = function() {
     form.addEventListener("submit", function(event) {
         event.preventDefault();
 
+        firstNameEle = document.getElementById("firstName");
+        lastNameEle = document.getElementById("lastName");
+        userEmailEle = document.getElementById("customerEmail");
+        userMSGEle = document.getElementById("customerMessage")
+
+        if (isValidEmail(userEmailEle.value) == false) {
+            alert("Invalid Email!")
+            return;
+        }
+
         const templateParams = {
-            name: document.getElementById("firstName").value,
-            email: document.getElementById("customerEmail").value,
-            message: document.getElementById("customerMessage").value
+            first: firstNameEle.value,
+            last: lastNameEle.value,
+            email: userEmailEle.value,
+            message: userMSGEle.value
         };
 
         emailjs.send("service_n95gjhb", "template_k0k9f9s", templateParams)
             .then(function(response) {
                 alert("Email success");
                 console.log("SUCCESS!", response.status, response.text);
+                firstNameEle.value = "";
+                lastNameEle.value = "";
+                userEmailEle.value = "";
+                userMSGEle.value = "";
             }, function(error) {
                 alert("Email failed");
                 console.error("FAILED...", error);
             });
     });
 };
+
+function isValidEmail(email) {
+    let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
 
 function addEvents() {
     let burger = document.getElementById("menu-image");
